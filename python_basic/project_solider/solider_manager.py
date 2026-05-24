a90258033923c7071036f3494c10f82c5aea6892
moty <g38257361@gmail.com>
2026-05-24 10:53:28 +0300

    added get_all_soliders func

--- python_basic/project_solider/solider_manager.py
+++ python_basic/project_solider/solider_manager.py
@@ -6,26 +6,32 @@ from data import solider_list_test
 
 
 
-
+"""הוספת חייל"""
 def add_solider(solider_id:int,solider_name:str):
     if find_by_id(solider_id):
         raise ValueError(f'id: {solider_id} exists in the system!')
     if not is_valid_name(solider_name):
-        raise ValueError(f'invalid name')
+        raise ValueError('invalid name')
     else:
         solider=({'id':solider_id,'name':solider_name,'duty':[{'n_duty':'','day':'','status':''}]})
         solider_list_test.append(solider)
 
 
 
+"""מחיקת חייל"""
+def remove_solider(id:int):
+    solider_exist = find_by_id(id)
+    if solider_exist:
+        solider_list_test.remove(solider_exist)
+    else:
+        raise KeyError(f':{id} not exist')
+
+
+
+def get_all_soliders():
+    for solider in solider_list_test:
+        print(solider)
 
 
 
 
-
-
-
-
-
-# add_solider(333,'moty')
-# print(solider_list_test)
